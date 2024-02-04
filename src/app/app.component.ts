f30fa53080f0843641a9480109ab74393700a95d
sheebu2004 <156988925+sheebu2004@users.noreply.github.com>
2024-02-04 20:00:07 +0530

    first_commit
    Date:04-02-2024

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-list-project';
+  constructor()
+    { 
+      // setTimeout(() => {
+      //   this.title='changed title'
+      // }, 2000);
+     }
+     
+}
